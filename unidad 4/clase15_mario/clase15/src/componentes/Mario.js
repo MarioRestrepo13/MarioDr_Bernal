8d9b9c880cdf220a2574affa4efc18fe5b03aa95
Mario <delgado.mario@correounivalle.edu.co>
2023-07-04 17:33:27 -0500

    tarea 15

---
+++ unidad 4/clase15_mario/clase15/src/componentes/Mario.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import {Row, Container} from "react-bootstrap";
+
+function Mario (){
+
+
+    return (
+        <Container>
+            <Row>
+                <div>
+                    Unidad 4
+               
+                </div>
+            </Row>
+        </Container>
+    )    
+}
+
+export default Mario;
